382cd94d1e055e5d2e1db6d2646c0dfb8e644638
lovelotus <opera.prem@gmail.com>
2022-07-30 12:19:40 +0530

    Added Create a Linked list By Adding UC2

--- DataSturucture/DataSturucture/LinkedList.cs
+++ DataSturucture/DataSturucture/LinkedList.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace DataStructure
 {
     public class LinkedList
     {
         internal Node head;
+        private Node temp;
         internal void Add(int data)
         {
             Node node = new Node(data);
@@ -40,6 +40,21 @@ namespace DataStructure
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
             }
+        }
+        internal void AddReverse(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.head = head;
+                head = node;
+                head.next = temp;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
         }
     }
 }
