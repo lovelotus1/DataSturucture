bd9cc8353fcd76b99183cdd3c508efc0b285e357
lovelotus <opera.prem@gmail.com>
2022-07-31 19:58:42 +0530

    Added Insert Methods UC4

--- DataSturucture/DataSturucture/LinkedList.cs
+++ DataSturucture/DataSturucture/LinkedList.cs
@@ -9,7 +9,7 @@ namespace DataStructure
     {
         internal Node head;
         private Node temp;
-        internal void Appending(int data)
+        internal void Add(int data)
         {
             Node node = new Node(data);
             if (this.head == null)
@@ -40,6 +40,40 @@ namespace DataStructure
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
             }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+                if (position < 1)
+                {
+                    Console.WriteLine("Invalid position");
+
+                    if (position == 1)
+                    {
+                        var newNode = new Node(data);
+                        newNode.next = this.head;
+                        head = newNode;
+                    }
+                    else
+                    {
+                        while (position-- != 0)
+                        {
+                            if (position == 1)
+                            {
+                                Node node = new Node(data);
+                                node.next = this.head.next;
+                                head.next = node;
+                                break;
+                            }
+                            head = head.next;
+                        }
+                        if (position != 1)
+
+                            Console.WriteLine("Position out of range");
+                    }
+
+                }
+            Console.WriteLine("Inserted value is : " + data);
+            return head;
         }
     }
 }
--- DataSturucture/DataSturucture/Program.cs
+++ DataSturucture/DataSturucture/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace DataStructure
 {
     class program
@@ -11,15 +10,13 @@ namespace DataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure Problem Program!");
-
             Console.WriteLine("Search Operation !");
             LinkedList list = new LinkedList();
-            list.Appending(56);
-            list.Appending(30);
-            list.Appending(70);
+            list.Add(56);
+            list.Add(70);
             Console.WriteLine("Display Nodes Are:");
-
             list.Display();
+            list.InsertAtParticularPosition(2, 30);
             Console.WriteLine("============================================");
             Console.ReadLine();
         }
